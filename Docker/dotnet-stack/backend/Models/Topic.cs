22bee3e7c832f5b0f527c684e47528f412d760d3
jornkbz <104862696+jornkbz@users.noreply.github.com>
2024-12-12 20:24:07 +0100

    actualizado

---
+++ Docker/dotnet-stack/backend/Models/Topic.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace backend.Models
+{
+    public class Topic
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("topicName")]
+        public string TopicName { get; set; }
+    }
+}
